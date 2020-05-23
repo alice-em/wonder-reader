d9b65427ed47494c639b400a7555e5bfd3858a8f
alice maldonado <the.alice.maldonado@gmail.com>
2020-05-23 01:08:36 -0500

    snapshots

--- app/components/Library/ControlBar.js
+++ app/components/Library/ControlBar.js
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React from 'react';
 
 import * as Buttons from './Buttons';
 
@@ -10,4 +10,4 @@ const ControlBar = () => (
   </div>
 );
 
-export default memo(ControlBar);
+export default ControlBar;
